2d750ca5f674167ef1c915d6a020763dc83146c5
yjie28 <xieyy28@gmail.com>
2020-02-26 22:18:02 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,121 @@
+const firstNumber = document.getElementById("first-number");
+const secondNumber = document.getElementById("second-number");
+const thirdNumber = document.getElementById("third-number");
+const fourthNumber = document.getElementById("fourth-number");
+
+const container = document.querySelector(".container");
+
+const guessButton = document.getElementById("guess");
+const startButton = document.getElementById("start");
+
+
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+let random4Nums;
+
+// shuffles numbers in the array (0 - 9)
+const shuffle = (arr) => {
+  let i = arr.length, j = 0, temp;
+
+  while (i--) {
+    j = Math.floor(Math.random() * (i + 1));
+
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr.slice(0, 4);
+}
+
+
+const startGame = (e) => {
+  container.style.display = "";
+  guessButton.style.display = "block";
+  startButton.style.display = "none";
+
+  // generates the 4 random numbers that player is going to guess
+  random4Nums = shuffle(arr);
+  console.log(random4Nums);
+}
+
+
+// checks if the number is within 1 - 8
+const checkRange = (num) => {
+  return (num > 0 && num < 9) ? true : false;
+}
+
+const incrementNumber = (e) => {
+  let currentNumber;
+
+  if (e === "first-number") {
+    currentNumber = Number(firstNumber.innerHTML);
+    if (checkRange(currentNumber) || currentNumber === 0) {
+      currentNumber += 1;
+      firstNumber.innerHTML = currentNumber;
+    }
+  } else if (e === "second-number") {
+    currentNumber = Number(secondNumber.innerHTML);
+    if (checkRange(currentNumber) || currentNumber === 0) {
+      currentNumber += 1;
+      secondNumber.innerHTML = currentNumber;
+    }
+  } else if (e === "third-number") {
+    currentNumber = Number(thirdNumber.innerHTML);
+    if (checkRange(currentNumber) || currentNumber === 0) {
+      currentNumber += 1;
+      thirdNumber.innerHTML = currentNumber;
+    }
+  } else if (e === "fourth-number" || currentNumber === 0) {
+    currentNumber = Number(fourthNumber.innerHTML);
+    if (checkRange(currentNumber) || currentNumber === 0) {
+      currentNumber += 1;
+      fourthNumber.innerHTML = currentNumber;
+    }
+  }
+}
+
+const decrementNumber = (e) => {
+  let currentNumber;
+
+  if (e === "first-number") {
+    currentNumber = Number(firstNumber.innerHTML);
+    if (checkRange(currentNumber) || currentNumber === 9) {
+      currentNumber -= 1;
+      firstNumber.innerHTML = currentNumber;
+    }
+  } else if (e === "second-number") {
+    currentNumber = Number(secondNumber.innerHTML);
+    if (checkRange(currentNumber) || currentNumber === 9) {
+      currentNumber -= 1;
+      secondNumber.innerHTML = currentNumber;
+    }
+  } else if (e === "third-number") {
+    currentNumber = Number(thirdNumber.innerHTML);
+    if (checkRange(currentNumber) || currentNumber === 9) {
+      currentNumber -= 1;
+      thirdNumber.innerHTML = currentNumber;
+    }
+  } else if (e === "fourth-number") {
+    currentNumber = Number(fourthNumber.innerHTML);
+    if (checkRange(currentNumber) || currentNumber === 9) {
+      currentNumber -= 1;
+      fourthNumber.innerHTML = currentNumber;
+    }
+  }
+}
+
+const guessNumber = () => {
+  let firstNumberGuessed = Number(firstNumber.innerHTML);
+  let secondNumberGuessed = Number(secondNumber.innerHTML);
+  let thirdNumberGuessed = Number(thirdNumber.innerHTML);
+  let fourthNumberGuessed = Number(fourthNumber.innerHTML);
+
+  if (firstNumberGuessed === random4Nums[0] &&
+    secondNumberGuessed === random4Nums[1] &&
+    thirdNumberGuessed === random4Nums[2] &&
+    fourthNumberGuessed === random4Nums[3]) {
+      console.log("Congrats you guessed the right number!!!");
+    } else {
+      console.log("Please try again!");
+    }
+}
